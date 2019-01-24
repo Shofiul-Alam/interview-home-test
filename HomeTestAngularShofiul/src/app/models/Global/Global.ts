16b94da12e854aad270d871836c3e5eaf2de8e5f
Shofiul Alam <shofi.tafe@gmail.com>
2019-01-25 07:41:41 +1100

    AngularAPIEndPointUpdate-25-Jan

--- HomeTestAngularShofiul/src/app/models/Global/Global.ts
+++ HomeTestAngularShofiul/src/app/models/Global/Global.ts
@@ -1,3 +1,3 @@
-// export const url = 'http://localhost:5000/api';
-export const url = 'https://shofiul.azurewebsites.net/api';
+export const url = 'http://localhost:5000/api';
+// export const url = 'https://shofiul.azurewebsites.net/api';
 
