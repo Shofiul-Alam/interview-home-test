0ef8af22d68be4b4955d412448e050fa2ac2927b
Shofiul Alam <shofi.tafe@gmail.com>
2019-01-25 12:01:16 +1100

    AngularAPIEndPointUpdate-25-Jan-Unit-Testing-Done

---
+++ UnitTest/ContactTest.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using HomeTestShofiul.Controllers;
+using HomeTestShofiul.Data;
+using HomeTestShofiul.Dtos;
+using HomeTestShofiul.Helpers;
+using HomeTestShofiul.Models;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class ContactTest
+    {
+        private ContactForAddDto _contactForAddDto;
+        private ContactForUpdateDto _contactForUpdateDto;
+        private IDataService<Contact> _contactService;
+        private IMapper _mapper;
+
+        public ContactTest()
+        {
+            _contactService = new DataService<Contact>();
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfiles());
+            });
+            _mapper = mockMapper.CreateMapper();
+
+
+
+            _contactForAddDto = new ContactForAddDto
+            {
+                Name = "Test Contact",
+                Address = "1 See Stree MedowBank College",
+                Details = "This is the testing details for ContactDto"
+            };
+            _contactForUpdateDto = new ContactForUpdateDto
+            {
+                Name = "Updating The Contact for testing",
+                Address = "Updated Address 1 See Stree MedowBank College",
+                Details = "This is the testing checking for updating the details",
+ 
+            };
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+
+
+        }
+
+        [Test]
+        public void GetContact()
+        {
+            var contactsController = new ContactsController(_contactService, _mapper);
+            var result = contactsController.GetContact(5).Result;
+
+            var okResult = result as OkObjectResult;
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void AddContact()
+        {
+            var contactsController = new ContactsController(_contactService, _mapper);
+            Task<IActionResult> result = contactsController.Add(_contactForAddDto);
+
+            Assert.That(result.IsCompleted, Is.True);
+        }
+
+        [Test]
+        public void UpdateContact()
+        {
+            var contactsController = new ContactsController(_contactService, _mapper);
+            var result = contactsController.UpdateContact(8, _contactForUpdateDto).Result;
+            var okResult = result as OkObjectResult;
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+
+       
+
+        [Test]
+        public void DeleteContact()
+        {
+            var contactsController = new ContactsController(_contactService, _mapper);
+            var result = contactsController.Delete(2);
+
+            var okResult = result as OkObjectResult;
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+    }
+}
--- UnitTest/ProjectTest.cs
+++ UnitTest/ProjectTest.cs
@@ -1,27 +1,99 @@
+using AutoMapper;
+using HomeTestShofiul.Controllers;
+using HomeTestShofiul.Data;
+using HomeTestShofiul.Dtos;
+using HomeTestShofiul.Helpers;
 using HomeTestShofiul.Models;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace UnitTest
 {
     [TestFixture]
     public class ProjectTest
     {
-        [SetUp]
-        public void CreateProject()
+        private ProjectForAddDto _project;
+        private ProjectForUpdateDto _projectUpdateObj;
+        private IDataService<Project> _projectService;
+        private IMapper _mapper;
+
+        public ProjectTest()
         {
+            _projectService = new DataService<Project>();
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfiles());
+            });
+            _mapper = mockMapper.CreateMapper();
+            
+
+
+            _project = new ProjectForAddDto
+            {
+                ProjectName = "Test Project",
+                Address = "1 See Stree MedowBank College",
+                Phone = "029988889",
+                ProjectDetails = "This is a Unit Test project Object"
+            };
+            _projectUpdateObj = new ProjectForUpdateDto
+            {
+                ProjectName = "Test Project updated Last",
+                Address = "1 See Stree MedowBank College",
+                Phone = "029988889",
+                ProjectDetails = "This is a Unit Test project Object",
+                ContactList = new List<int>(){ 2, 3 }
+            };
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+
+           
+        }
+
+        [Test]
+        public void AddProject()
+        {
+            var projectController = new ProjectsController(_projectService, _mapper);
+            Task<IActionResult> result = projectController.Add(_project);
+
+            Assert.That(result.IsCompleted, Is.True);
         }
 
         [Test]
         public void UpdateProject()
         {
-            
-            Assert.That("", Is.True);
+            var projectController = new ProjectsController(_projectService, _mapper);
+            var result = projectController.UpdateProject(4, _projectUpdateObj).Result;
+            var okResult = result as OkObjectResult;
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
         }
+
+        [Test]
+        public void GetProject()
+        {
+            var projectController = new ProjectsController(_projectService, _mapper);
+            var result = projectController.GetProject(5).Result;
+
+            var okResult = result as OkObjectResult;
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+
         [Test]
         public void DeleteProject()
         {
+            var projectController = new ProjectsController(_projectService, _mapper);
+            var result = projectController.Delete(3);
 
-            Assert.That("", Is.True);
+            var okResult = result as OkObjectResult;
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
         }
     }
 }
